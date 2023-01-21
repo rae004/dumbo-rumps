e0728a6e10ac4e139f03adb545e07801f0154d9e
Robert Engel <robert.engel@apollidon.com>
2023-01-21 17:12:01 -0500

    Starting basic animations

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,33 @@ import React from 'react';
 import './App.css';
 import ScrollAnimation from 'react-animate-on-scroll';
 import 'animate.css/animate.min.css';
-
+import rump1 from './rumpHeroSmall.webp';
+import { FiArrowDown } from 'react-icons/fi';
 function App() {
     return (
         <div className="App">
             <header className={'scroll-container'}>
-                <ScrollAnimation
-                    initiallyVisible={true}
-                    animatePreScroll={true}
-                    className={'animated-text'}
-                    animateIn={'animate__wobble'}
-                >
-                    In the Kruger National Park...
-                </ScrollAnimation>
-                <ScrollAnimation>{/*<img src={}>*/}</ScrollAnimation>
+                <div className={'hero-row'}>
+                    <FiArrowDown className={'hero-down-arrows'} />
+                    <span>SCROLL DOWN</span>
+                    <FiArrowDown className={'hero-down-arrows'} />
+                </div>
             </header>
-
+            <div className={'scroll-container'}>
+                <ScrollAnimation
+                    animateIn={'animate__backInRight'}
+                    animateOut={'animate__backOutLeft'}
+                >
+                    <img src={rump1} />
+                </ScrollAnimation>
+                <ScrollAnimation
+                    animateIn={'animate__backInRight'}
+                    animateOut={'animate__backOutLeft'}
+                    className={'animated-text'}
+                >
+                    You encounter a lotta RUMPS!
+                </ScrollAnimation>
+            </div>
             <div className={'scroll-container'}>
                 <ScrollAnimation
                     className={'animated-text'}
